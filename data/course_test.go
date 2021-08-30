a93c13c6b16838129acaafcebbfceac872beaec6
KaseyColeman <kcoleman@student.neumont.edu>
2021-08-29 18:56:47 -0600

    Update course_test.go

--- data/course_test.go
+++ data/course_test.go
@@ -1,1 +1,23 @@
 package data
+
+import "testing"
+
+func TestCourseGet(t *testing.T) {
+	arry := GetCourses()
+	if arry ==nil{
+ 		t.Errorf("Time to panic now")
+	}
+}
+
+func TestSingleCourseGet(t *testing.T) {
+	_, err := GetCourseByID(4)
+	if err == nil{
+ 		t.Errorf("Time to panic now")
+	}
+}
+
+
+
+
+
+
